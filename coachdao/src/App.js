350ae673a03eec1f30966b1735476d62b1873ab3
Jaycus <46542516+Jaycus@users.noreply.github.com>
2022-12-16 21:24:30 +0700

    third commit

---
+++ coachdao/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>CoachingDAO</h1>
+      <div>
+        <h2>Smart Contract Details</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
